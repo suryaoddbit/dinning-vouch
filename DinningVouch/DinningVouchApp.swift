c648815e0ad38ca62f2ae8d6bcaf0f1e910e5863
Surya Adi <wayan.surya@stockbit.com>
2022-06-05 13:20:34 +0800

    init project

---
+++ DinningVouch/DinningVouchApp.swift
@@ -0,0 +1,17 @@
+//
+//  DinningVouchApp.swift
+//  DinningVouch
+//
+//  Created by I Wayan Surya Adi Yasa on 05/06/22.
+//
+
+import SwiftUI
+
+@main
+struct DinningVouchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
